e8e1c0cd7602b3264a9296e3efde39335638ca1b
PixelByte24 <paranjoyhazarika@gmail.com>
2025-06-05 02:47:34 +0530

    Updated the inventory program

--- Simple Inventory Checker/simple_inventory.py
+++ Simple Inventory Checker/simple_inventory.py
@@ -2,11 +2,27 @@ try:
     inventory = ["banana", "apple"]
     user_list = []
 
-    items_list = int(input("Enter the number of items (in numbers): "))
+    while True:
+        try:
+            items_list = int(input("Enter the number of items (in numbers): "))
+            if items_list <= 0:
+                print("Please enter a positive number")
+                continue
+            break
+            
+        except ValueError:
+            print("Please enter a valid number")
+            continue
 
     for number in range(1, items_list + 1):
-        user_item = input(f"{number}. ")
-        user_list.append(user_item.lower())
+        while True:
+            user_item = input(f"{number}. ")
+            if user_item:
+                user_list.append(user_item.lower())
+                break
+            else:
+                print("Please enter a valid input")
+                continue
 
     available_items = []
     unavailable_items = []
@@ -20,11 +36,11 @@ try:
     if (available_items):
         print("\nAvailable items:")
         for item in available_items:
-            print(f"- {item}")
+            print(f"- {item.capitalize()}")
     if (unavailable_items):
         print("\nUnavailable items:")
         for item in unavailable_items:
-            print(f"- {item}")
+            print(f"- {item.capitalize()}")
 
 except ValueError:
     print("Error: Please enter a valid number for the list of items")
