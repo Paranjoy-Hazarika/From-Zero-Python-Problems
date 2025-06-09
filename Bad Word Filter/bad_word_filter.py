8137cd52897ae4808564d34fb9ccbaea9c02987a
PixelByte24 <paranjoyhazarika@gmail.com>
2025-06-09 17:54:56 +0530

    Wrote the bad word filter code

---
+++ Bad Word Filter/bad_word_filter.py
@@ -0,0 +1,59 @@
+import string
+
+def filter_word(sentence: str, bad_words: set) -> str:
+    
+    filtered_words = []
+    for words in sentence.split():
+        clean_word = words.strip(string.punctuation)
+
+        if clean_word.lower() in bad_words:
+            filtered_words.append("*" * len(clean_word))
+        else:
+            filtered_words.append(clean_word)
+
+    
+    return " ".join(filtered_words)
+
+
+def main():
+    bad_words = {"fuck", "bastard"}
+
+    print("BAD WORD FILTER")
+    
+    while True:
+        try:
+            print("Menu")
+            print("1. Filter Words")
+            print("2. Exit")
+
+            user_choice = int(input("Enter your choice: "))
+
+            match user_choice:
+                case 1:
+                    while True:
+                        sentence = input("Enter your sentence: ")
+                        
+                        if not sentence.strip():
+                            print("Please enter some text")
+                            continue
+
+                        cleaned_words = filter_word(sentence, bad_words)
+                        print(f"Filtered text: \n{cleaned_words}")
+                        break
+                case 2:
+                    print("Quiting...")
+                    break
+                case _:
+                    print("Please enter a valid option (1 or 2)")
+                    continue
+
+        
+        except ValueError:
+            print("Please enter a number")
+        except KeyboardInterrupt:
+            print("\nTerminated by user")
+            break
+
+
+if __name__ == "__main__":
+    main()
